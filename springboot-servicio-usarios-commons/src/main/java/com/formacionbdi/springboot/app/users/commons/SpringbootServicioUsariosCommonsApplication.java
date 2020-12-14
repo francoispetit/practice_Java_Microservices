fecaeb65b88b9d387c25cf1620a54774199355a8
FraPe <francois.petit@outlook.com>
2020-12-14 19:11:48 +0100

    added common service for users

---
+++ springboot-servicio-usarios-commons/src/main/java/com/formacionbdi/springboot/app/users/commons/SpringbootServicioUsariosCommonsApplication.java
@@ -0,0 +1,12 @@
+package com.formacionbdi.springboot.app.users.commons;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class SpringbootServicioUsariosCommonsApplication {
+
+
+}
